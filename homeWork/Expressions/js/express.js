85c594e2a5c5b3e45df0053aaad2beb4d766fd84
BirchardEricAvery <eabirchard@fullsail.edu>
2015-04-03 18:35:47 -0400

    Modulo

--- homeWork/Expressions/js/express.js
+++ homeWork/Expressions/js/express.js
@@ -12,22 +12,37 @@ console.log(a); //logging a to output
 
 //year of birth
 
-var yearBorn = 1969; //
+var yearBorn = 1969;            //my year of birth
 
-var age = 2015 - yearBorn - 1;
+var age = 2015 - yearBorn - 1; //year of birth minus 1
 
-console.log("I am" + " " + age + " " + "years old");
+console.log("I am" + " " + age + " " + "years old"); //logging my age to screen
 
 //arithmetic operators
 
-var width = prompt("enter width");
-Number(width);
+var width = prompt("enter width");  //prompt for value of width
+Number(width);                      //convert string to number
 
-var height = prompt("Enter height of triangle");
-Number(height);
+var height = prompt("Enter height of triangle");    //prompt for height
+Number(height);                                     //convert string to number
 
-var area = width * height / 2;
+var area = width * height / 2;                      //variable math
 
-console.log("The area of a triangle is " + " " + area);
+console.log("The area of a triangle is " + " " + area); //log area of a triage to screen
+
+
+//Modulo operator % for remainder
+
+var number1 //declare variable
+var number2 //declare variable
+var number1 = prompt("Enter a numbers to divide for remainder" + number1);  //first number
+var number2 = prompt("Enter second number" + number2);                        //prompt for values of variables
+var remainder
+
+Number(number1, number2);
+
+remainder = number1 % number2;
+
+console.log("The remainder is" + " " + remainder);
 
 
