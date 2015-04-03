e9b1cf6a0902798288a0502cc67b52f63befa670
BirchardEricAvery <eabirchard@fullsail.edu>
2015-04-03 17:45:20 -0400

    re doing variable hw

---
+++ homeWork/Expressions/js/express.js
@@ -0,0 +1,12 @@
+/*  Name:       Eric Avery Birchard
+    Assignment: Expressions homework
+    Date:       3, April 2015
+    Class:      SDI
+    Term:       I
+ */
+                //declare and assign variable
+var a = 2;      //defining a
+a = a+ 3;       //assigning or storing values to a
+
+console.log(a); //logging a to output
+
