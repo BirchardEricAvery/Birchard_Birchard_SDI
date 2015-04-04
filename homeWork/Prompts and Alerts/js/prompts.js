e3a02038ee270d148ea1c65c995d92e29c7a2797
BirchardEricAvery <eabirchard@fullsail.edu>
2015-04-03 17:56:52 -0700

    Prompts and alerts

---
+++ homeWork/Prompts and Alerts/js/prompts.js
@@ -0,0 +1,25 @@
+/*  Name:       Eric Avery Birchard
+    Assignment: Expressions homework
+    Date:       3, April 2015
+    Class:      SDI
+    Term:       I
+ */
+
+//Prompts
+
+
+var width = prompt("Enter width for rectangle");    //using prompt to gather variable input
+var width = Number(width);                          //Casting variable
+
+var height = prompt("Enter height for rectangle");   //using prompt to gather variable input
+var height = Number(height);                        //Casting variable
+
+var area = width * height;
+console.log("The width of the rectangle is" + " " + area);
+
+
+//alert
+
+alert("The area of your rectangle is" + " " + area);
+
+//video died, not sure why, but I get it.
