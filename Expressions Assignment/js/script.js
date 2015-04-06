cd04974fa8688e5f508b203acd66e2cf2e5d21f1
BirchardEricAvery <eabirchard@fullsail.edu>
2015-04-05 23:12:51 -0700

    Stump coding and ideas complete

--- Expressions Assignment/js/script.js
+++ Expressions Assignment/js/script.js
@@ -15,84 +15,89 @@
 var startingBal;                    //starting balance (numerical)
 var minusTotal;                     //total negative (numerical
 var posTotal;                       //total positive (numerical)
+var running;                        //running balance (numerical)
+var tenDep                          //list out deposits (numerical) array
+var tenWithdraws                    //list out withdraws (numerical) array
 var name;                           //name variable (string)
 var phone;                          //phone number as a string
 var address;                        //address (string)
 var estIntM;                        //estimated interest rate monthly
 var estIntY                         //estimated interest rate if you balance averages...(numerical)
+var int                             //interest rate
+var numberDep; var numberWithdraws  //running count of deposits and withdraws
 
+var w; var w1; var w2; var w3; var w4; var w5; var w6; var w7; var w8; var w9; //for ten withdraws
+var d; var d1; var d2; var d3; var d4; var d5; var d6; var d7; var d8; var d9; //for ten deposits
 
-//interest rate for average balance
-//increment entry's
-//2 examples of arithmetic operators + and -, /, *
-//example of an array used as part of the calculation
-//string variable as output
-//2 assignment operators += -= *= /= -- ==
 
 //TODO: prompts
-//data types number, string, boolean, array
+//data types number, string, array
 
-vehicleType = prompt("What type of vehicle do you drive?");              //prompt for string input from user
+//Contact information
+name = prompt("Enter your name");                           //account holders name
+console.log("Your name is:" + " " + name);                  //console logging name
 
-var startingGallons = prompt("Starting fuel in tank");                   //prompt for number input from user
+address = prompt("enter your address:" + " " + address);    //account holders address on one line
+console.log("Residence:" + " " + address);                  //console logging address
 
-var milesDriven = prompt ("How many miles have you driven so far");      //prompt for number input for miles driven
-
-var fillUp = prompt ("How many gallons did it take to fill your tank?"); //prompt for number of gallons it took to fill tank
-
-// re-var, meaning my vars to shorter vars for calculations
-
-V = vehicleType;        //Vehicle type is now V
-
-sG = startingGallons;   //Starting Gallons is now SG
-
-mD = milesDriven;       //Miles driven is now mD
-
-fU = fillUp;            //fillUp is now FU
+phone = prompt("Enter contact number:" + " " + phone);      //contact phone number
+console.log("Phone number:" + " " + phone);                 //console logging phone number
 
 
-// Calculations for MPG and gallons used
+//Starting balance
+startingBal = prompt("Enter your staring balance");                 //prompt for string input from user
+var startingBal = Number(startingBal);                              //casting string to a number
 
-gU = sG - fU;      //Gallons used arithmetic operation 2
+//Interest rate
+int = prompt("enter your annual percentage rate:");                 //interest to be calculated on average balance
+var int = Number(int);                                              //casting int string to a number
+
+//Console logging balance and interest rate
+console.log("Your staring balance is:" + " " + startingBal);        //console logging starting balance
+console.log("Your APR is:" + " " + int);                            //console logging APR
 
 
-mPG = mD/ fU;      //MPG calculation with parentheses prioritization arithmetic operation 2
-
-
-
-//Your MPG is: (type of concatenation)
-
-alert("Your" + " " + V + " " + "MPG" + " " + "is:" + " " + mPG + " " + "using" + " " + gU + " " + "Gallons");
-
-isThereGasLeftOver = true;  //boolean value
+//increment entry's
+//2 examples of arithmetic operators + and -, /, *
+//2 assignment operators += -= *= /= -- ==
 
 //array data variable outputs
-var myArray;
-myArray =  new Array();         //array variable
+//example of an array used as part of the calculation
+/*
+
+var tenDep[];
+
+myArray();                      //array variable
 myArray[0] = V + " ";
 myArray[1] = mPG + " " + "and" + " ";
 myArray[2] = gU + ".";
 
-alert("Types of data used in this program:" + " " + myArray);
+var personnel = new Array();
+
+personnel[0] = new Array();
+personnel[0][0] = "Name0";
+personnel[0][1] = "Age0";
+personnel[0][2] = "Address0";
+
+personnel[1] = new Array();
+personnel[1][0] = "Name1";
+personnel[1][1] = "Age1";
+personnel[1][2] = "Address1";
+
+personnel[2] = new Array();
+personnel[2][0] = "Name2";
+personnel[2][1] = "Age2";
+personnel[2][2] = "Address2";
+
+console.log("Name : " + personnel[0][0] + personnel[1][0] + personnel[2][0] + "\r");
+console.log("Age : " + personnel[0][1] + personnel[1][1] + personnel[2][1] + "\r");
+console.log("Address : " + personnel[0][2] + personnel[1][2] + personnel[2][2] + "\r");
+//string variable as output
 
 
+//interest rate for average balance monthly and yearly
 
-//TODO: console logs //with descriptive sentences
 
-console.log("vehicleType value" + " " + vehicleType);                       //string value for vehicle type
-console.log("V value" + " " +V);                                            //assign sting to V for shorter reference
-console.log("startingGallons value" + " " + startingGallons);               //string input for starting gallons
-console.log("sG value" + " " + sG);                                         //assign string to SG for numerical value
-console.log("milesDriven value" + " " +milesDriven);                        //string value for miles driven
-console.log("mD value" + " " + mD);                                         //assign string to MD for numerical value
-console.log("fillUp value" + " " + fillUp);                                 //string variable for initial fill up.
-console.log("fU value" + " " + fU);                                         //assign fillUp to FU for numerical value
-console.log("isThereGasLeftOver value" + " " + isThereGasLeftOver);         //assign boolean value true to variable is ThereGasLeftOver
-console.log("gU value" + " " + gU);                                         //Gallons used variable
-console.log("mPG value" + " " + mPG);                                       //MPG variable for miles per gallon calculation
-console.log("myArray values" + " " + myArray[0], myArray[1], myArray[2]);   //array for console list purposes
-
-/*
  Input type of car driven, in this cas it's a car.
 
  Input starting gallons in tank, that being 10.
