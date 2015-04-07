ee49e30e9b60e3fb412c249a3064badb597579a5
BirchardEricAvery <eabirchard@fullsail.edu>
2015-04-07 01:00:08 -0700

    Check book math error free
    copy past next, final math, then array.

--- Expressions Assignment/js/script.js
+++ Expressions Assignment/js/script.js
@@ -32,40 +32,66 @@ var d; var d1; var d2; var d3; var d4; var d5; var d6; var d7; var d8; var d9; /
 
 //Contact information
 //TODO: cast as strings
- var name = prompt("Enter your full name");                             //account holders name
-name = String(name);
-console.log("Your name is:" + " " + name);                              //console logging name
 
-var address = prompt("enter your complete address:" + " " + address);    //account holders address on one line
-address = String(address);
-console.log("Residence:" + " " + address);                      //console logging address
+var name = prompt("Enter your full name");                              //account holders name
+String(name);
+console.log("Name:" + " " + name);                                      //console logging name
 
-var phone = prompt("Enter contact number:" + " " + phone);      //contact phone number
-phone = String(phone);
-console.log("Phone number:" + " " + phone);                     //console logging phone number
+var address = prompt("enter your complete address:");    //account holders address on one line
+String(address);
+console.log("Residence:" + " " + address);                              //console logging address
+
+var phone = prompt("Enter contact number:");              //contact phone number
+String(phone);
+console.log("Phone number:" + " " + phone);                             //console logging phone number
 
 //Starting balance
-startingBal = prompt("Enter your staring balance");                 //prompt for string input from user
-var startingBal = Number(startingBal);                              //casting string to a number
+startingBal = prompt("Enter your staring balance");                     //prompt for string input from user
+var startingBal = Number(startingBal);                                  //casting string to a number
 console.log("Starting balance" + " " + startingBal);
 
 
-//increment entry's
-//2+ assignment operators += -= *= /= -- ==
-//TODO: checkbook math for first withdraw and deposit
+
+
+//TODO: checkbook math for withdraws and deposits
+
 //1 Withdraw 1 Deposit...
+//assignment operators += -= *= /= -- ==
 w = prompt("Enter amount of withdraw:");                                    //First withdraw
-w = Number(w);                    //casting var w string to a number
-console.log("Withdraw:" + " " + w);
-running = startingBal - w;      //running balance withdraw
-console.log("Balance after withdraw:" + " " + running );
+w = Number(w);                                                              //casting var w string to a number
+console.log("Withdraw:" + " " + w);                                         //running total math
+running = startingBal - w;                                                  //running balance withdraw
+console.log("Balance after withdraw:" + " " + running );                    //balance after withdraw
 
 d = prompt("End amount of deposit \n If not deposit enter the number 0:");  //First deposit
-d = Number(d);                    //casting var d string to number
-console.log("Deposit:" + " " + d);
+d = Number(d);                                                              //casting var d string to number
+console.log("Deposit:" + " " + d);                                          //deposit
 
-var run = running + d; console.log("Balance after deposit:" + " " + run);
+running += d; console.log("Balance after deposit:" + " " + running);        //balance after deposit
 
+//increment entry's
+incWith++;                      //decrementing for number of withdraws, 1
+incDep++;                       //incrementing for number of deposits,  1
+
+console.log("Withdraw:" + " " + w + "\n" + "Deposit:" + " " + d);               //Amount of withdraw and deposit
+console.log("Withdraws" + " " + incWith + "\n" + "Deposits" + " " + incDep);    //increments and decrements
+
+
+//2 Withdraw 2 Deposit...
+//assignment operators += -= *= /= -- ==
+w1 = prompt("Enter amount of withdraw:");                                       //First withdraw
+w1 = Number(w1);                                                                //casting var w string to a number
+console.log("Withdraw:" + " " + w1);                                            //running total math
+running -= w1;                                                                  //running balance withdraw
+console.log("Balance after withdraw:" + " " + running );                        //balance after withdraw
+
+d1 = prompt("End amount of deposit \n If not deposit enter the number 0:");     //First deposit
+d1 = Number(d1);                                                                //casting var d string to number
+console.log("Deposit:" + " " + d1);                                             //deposit
+
+running += d1; console.log("Balance after deposit:" + " " + running);           //operator for balance after deposit
+
+//increment entry's
 incWith++;                      //decrementing for number of withdraws, 1
 incDep++;                       //incrementing for number of deposits,  1
 
@@ -74,15 +100,19 @@ console.log("Withdraws" + " " + incWith + "\n" + "Deposits" + " " + incDep); //i
 
 
 
+
+
 //2+ examples of arithmetic operators + and -, /, *
 
 //check book calculations
+
 
 
 //Interest rate
 int = prompt("enter your annual percentage rate:");                     //interest to be calculated on average balance
 int = Number(int);                                                      //casting int string to a number
 
+//assignment operator += -= *= /= -- ==
 console.log("Your staring balance is:" + " " + startingBal);            //console logging starting balance
 console.log("Your APR is:" + " " + int + "%");                          //console logging APR
 int *= .01; console.log("Interest rate for computation" + " " + int);   //interest rate for computation
