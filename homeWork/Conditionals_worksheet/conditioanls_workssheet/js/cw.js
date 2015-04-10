b1793793b13ecb5c9d08c1d4b9ce34afe64458be
BirchardEricAvery <eabirchard@fullsail.edu>
2015-04-10 10:50:56 -0700

    Stuff your face done

--- homeWork/Conditionals_worksheet/conditioanls_workssheet/js/cw.js
+++ homeWork/Conditionals_worksheet/conditioanls_workssheet/js/cw.js
@@ -5,3 +5,30 @@
     Class:      SDI
     Term:       I
 */
+
+//Stuff your face, do you weigh enough to enter the pie eating contest?
+
+//variables
+
+var weight;     //weight variable
+var toLight;    //variable to hold to light to enter
+var enter;      //variable to hold weighty enough to enter
+
+//strings
+
+toLight = "You need to gain weight in order to enter the contest.  " + "\n" +
+    "Eat more tasty things!";                                       //To light to enter
+
+enter = "Your weighty enough, may you eat your way to the top!";    //weighty enough to enter
+
+
+weight = prompt("Please enter you weight to determine if you qualify to enter the pie eating contest" +
+    "\n" + "Please enter numbers only");                            //enter variable for weight
+
+Number(weight);                                                     //cast weight string to a number
+
+weight >= 250 ? console.log(enter) : console.log(toLight);
+
+
+
+
