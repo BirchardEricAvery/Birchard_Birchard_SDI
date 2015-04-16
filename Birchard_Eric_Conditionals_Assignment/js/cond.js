f0b43f69a9edb4e9197bb2a34a7c387b23db1788
BirchardEricAvery <eabirchard@fullsail.edu>
2015-04-15 22:45:29 -0700

    Basic conditional structure complete
    finish tomorrow I hope

--- Birchard_Eric_Conditionals_Assignment/js/cond.js
+++ Birchard_Eric_Conditionals_Assignment/js/cond.js
@@ -1,32 +1,157 @@
 /*  Name:       Eric Avery Birchard
     Instructor: Dan Williams
     Assignment: Conditionals Assignment
-    Date:       10, April 2015
+    Date:       15, April 2015
     Class:      SDI
     Term:       I
 
 */
 
-//conditionals assignment
 
-//TODO idea!!!
-//Bank loan
+//conditionals assignment
+//variables
+var income;
+var loanMax;
+var debt;
+var jobLength;
+var residence;
+var creditScore;
+var score;
+
+//Credit Score Calc
+var reEnter;        //851+              reEnter data for value between 0 and 850      
+var diamond;        //800 - 850         2%
+var platinum;       //700 - 799         4%
+var gold;           //650 - 699         12%
+var silver;         //550 - 649         18%
+var bronze;         //450 -             24%
+var bad;            //below 449         ReApply
+
+var rate;
+
+//TODO Bank loan
+
 //information: Name, address, phone
+
+//TODO: cast as strings
+//account holders name
+
+var name = prompt("Enter your full name");
+String(name);
+
+//console logging name
+console.log("Name:" + " " + name); //TODO begin validations......
+
+//account holders address on one line
+var address = prompt("enter your complete address:");
+String(address);
+
+//console logging address
+console.log("Residence:" + " " + address);
+
+//contact phone number
+var phone = prompt("Enter contact number:");
+String(phone);
+
+//console logging phone number
+console.log("Phone number:" + " " + phone);
+
+//Checking balance
+var checkingBal = prompt("Enter your staring balance");
+
+//casting string to a number
+var startingBal = Number(startingBal);
+
+//Checking Balance
+console.log("Checking balance:" + " " + "$" + checkingBal);
+
+//Other investments balance total
+var otherBal = prompt("Enter your any other investments holdings total liquid balance");
+
+//Other sources balance
+console.log("Total other sources balance:" + " " + "$" + otherBal);
+
+//array for info
+var info;
+info = [name, "\n", address, "\n", phone, "\n", checkingBal, "\n", otherBal, "\n"];
+
+//Display info
+alert(info);
+
+
 //income determines max
+income = prompt("What is your annual income?");
+
+//begin loan max calc
+loanMax = income;
+
 //debt to income ratio determines limiter via percent calc
+debt = prompt("What is your current debt?");
+//TODO: debt to income ratio and possible qualifier
+
+
 //time at current job determines length of loan
-//time at residence/ wait period,
+jobLength = prompt("How long have you been employed at your current employer?");
+
+
+//time at residence/ wait period
+residence = prompt("How long have you lived at your current residence?");
+
 //credit score determines interest rate
+creditScore = prompt("What is your current credit score?");
+
+
+
+//scores variables
+reEnter = "Invalid range, please enter a range between 0 and 850";
+diamond = 800;
+platinum = 700;
+gold = 650;
+silver = 550;
+bronze = 450
+bad = 449;
+
+//Credit score level
+
+if(creditScore > 851){score = reEnter
+
+}else if(creditScore <= -1){score = reEnter
+
+}else if(creditScore >= 800){score = diamond
+
+}else if(creditScore >= 700){score = platinum
+
+}else if(creditScore >= 650){score = gold
+
+}else if(creditScore >= 550){score = silver
+
+}else if(creditScore >= 450){score = bronze
+
+}else if(creditScore < 69){score = bad}
+
+//rate if statements
+
+if(score = diamond){rate = .02
+
+}else if(score = platinum){rate = .04
+
+}else if(score = gold){rate = .12
+
+}else if(score = silver){rate = .18
+
+}else if(score = bronze){rate = .24
+
+}else(score = bad);
+
 //payment calc
 
 //2 arithmetic operators + -,
-//2 conditional statements with an if statement, and not validating prompts
+//2 conditional statements with an if statement, and validating prompts
 //1 ternary conditional statement
 //1 logical operator &&, ||, !
-//3 prompts w/ validations
 
 
-//5+ commits
+
 //concatenation of strings and variables
 
 //values in multi-lines comment
@@ -37,67 +162,7 @@
 
 /*
 notes:
-//caparison
-temp = 82;
 
-
-if(temp > 70){
-    goToTheBeach();
-
-    if(warmWater) {
-        wearNewSuit();
-    }
-}else{
-    goToTheMovies();
-}
-
-
-
-
-
-//Conditional logic
-//If else
-
-//if the kid is over 48 inches in height
-
-var kidHeight = 48;
-var minHeight = 48;
-var sneakerLift = 2;
-
-//if the child is old enough, print to the console "you can ride!"
-
-if(kidHeight > minHeight){
-    //code performed if condition is true
-    console.log("You can ride the coaster!");
-    //code performed if condition is false
-} else{
-    console.log("Sorry kid you got some growing to do first!");
-
-}
-
-
-
-//Conditional logic
-//else if for three blocks of code always has a condition attached
-
-//if the kid is over 48 inches in height
-
-var kidHeight = 48;
-var minHeight = 48;
-var wParentHeight = 45;
-
-//if the child is old enough, print to the console "you can ride!"
-
-if(kidHeight > minHeight){
-    //you can ride!
-    console.log("You can ride the coaster!");
-    //you can ride with a parent present
-    //else should never have a conditions attached
-}else if(kidHeight > wParentHeight){
-    console.log("You can ride,but only with a parent present.");
-    //sorry kid you have some growing to do
-    console.log("Sorry kid you got some growing to do first!");
-}else{}
 
 Relational operators > < >= <= compare to result in true or false
 Equality operators
@@ -173,4 +238,5 @@ var book;
 book = (age < 10) ? "Green Eggs and Ham" : "The Time Machine";
 console.log(book);
 
+ //3 prompts w/ validations
  */
