d870b35f4d50197f81359219dbe442ba2642f2f8
BirchardEricAvery <eabirchard@fullsail.edu>
2015-04-22 22:28:25 -0700

    Final corrections done

--- Birchard_Eric_Conditionals_Assignment/js/cond.js
+++ Birchard_Eric_Conditionals_Assignment/js/cond.js
@@ -67,8 +67,7 @@ console.log("Name:" + " " + name);
 
 //account holders address
 address = prompt("enter your complete address:");
-if(address === "")
-{prompt("Entry was left blank, please enter your full address:")
+if(address === "") {prompt("Entry was left blank, please enter your full address:")
     }else{alert("Address:" + " " + address)}
 
 //console logging name
@@ -84,30 +83,37 @@ if(phone === "") {prompt("Entry was left blank, please make an entry:")
 console.log("Phone:" + " " + phone);
 
 
-
 //Checking balance
 checkingBal = prompt("Enter your checking account balance");
 
 if(checkingBal === "") {
     prompt("Entry was left blank, please enter your checking balance:")
-}else{alert("Checking balance:" + " " + "$" + checkingBal)}
+    }else if(isNaN(checkingBal)){checkingBal = prompt("Please enter numbers only for checking balance:")
+        }else{alert("Checking balance:" + " " + "$" + checkingBal)}
+
+//casting checkingBal to a number
+Number(checkingBal);
 
 //console logging name
 console.log("Checking balance:" + " " + "$" + checkingBal);
-Number(checkingBal);
+
 
 
 
 //Other investments balance total
 otherBal = prompt("Enter your any other investments holdings total liquid balance");
 
-if(otherBal === "") {
-    prompt("Entry was left blank, please enter liquid assets cash total:")
-}else{alert("Liquid cash assets total:" + " " + "$" + otherBal)}
+if(otherBal === ""){prompt("Entry was left blank, please enter liquid assets cash total:")
+    }else if(isNaN(otherBal)){otherBal = prompt("Please enter numbers only for liquid assets cash total:")
+        }else{alert("Liquid cash assets total:" + " " + "$" + otherBal)}
+
+//casting otherBal to a number
+Number(otherBal);
+
 
 //console logging other investments total
 console.log("Liquid assets cash total:" + " " + "$" + otherBal);
-Number(otherBal);
+
 
 
 //array for info
@@ -137,17 +143,19 @@ console.log("Name:" + " " + info[0] + "\n" +
 income = prompt("Enter your annual income in numbers only:");
 
 if(income === 0) {
-    prompt("Enter a number greater than Zero:")
+    income = prompt("Enter a number greater than Zero:")
 }else if(income === "") {
-    prompt("Entry was left blank, please enter a numebr:")
-}else if(isNaN(income)){prompt("Please enter numbers only:")
-}else{alert("Annual income:" + " " + "$" + income)}
-
-//console log income
-console.log("Income entered:" + " " + income);
+    income = prompt("Entry was left blank, please enter a number:")
+    }else if(isNaN(income)){income = prompt("Please enter numbers only for your annual income:")
+        }else{alert("Annual income:" + " " + "$" + income)}
 
 //casting income to a number
 Number(income);
+
+//console log income
+console.log("Income entered:" + " " + "$" + income);
+
+
 
 //begin loan max calculations with validation
 loanMax = income;
@@ -162,9 +170,12 @@ Number(debt);
 
 if(debt < 0){prompt("Please enter a number greater than a negative number:")
 }else if(debt === "") {
-    prompt("Entry left blank, please enter total debt")
-}else if(isNaN(debt)){prompt("Please enter numbers only:")
-}else{alert("Debt entered correctly:" + " " + "$" + debt)}
+    debt = prompt("Entry left blank, please enter total debt")
+    }else if(isNaN(debt)){debt = prompt("Please enter numbers only for your total debt:")
+        }else{alert("Debt entered correctly:" + " " + "$" + debt)}
+
+//Casting debt to a number
+Number(debt);
 
 //alert and console logging debt
 alert("Debt:" + " " + "$" + debt);
@@ -172,8 +183,7 @@ alert("Debt:" + " " + "$" + debt);
 //console log total debt
 console.log("Debt:" + " " + "$" + debt);
 
-//Casting debt to a number
-Number(debt);
+
 
 //Mathematical ratio
 ratio = debt/ income;
@@ -186,10 +196,6 @@ console.log("Your debt to income ratio is:" + " " + displayRatio + "%");
 //ternary
 displayRatio >= 60 ? qual = "Bring debt to income ratio below 60% to qualify for loan" :
     "Your debt to income ratio qualifies for the application process";
-
-//Final display for debt to income ratio
-alert(qual);
-console.log(qual);
 
 
 //TODO: End of debt to income code block********************************************************************************
@@ -204,18 +210,17 @@ console.log(qual);
 jobLength = prompt("How long  in months have you been employed at your current employer?");
 
 if(jobLength === ""){prompt("Entry left blank, please enter a number of months employed at current employer:")
-}else if(jobLength === 0){prompt("Please enter a number greater than Zero:")
-}else{alert("Current time at current employer in months:" + " " + jobLength)}
+    }else if(jobLength === 0) {
+    prompt("Please enter a number greater than Zero:")
+        }else if(isNaN(jobLength)){jobLength = prompt("Please enter job length in numbers only:")
+            }else{alert("Current time at current employer in months:" + " " + jobLength)}
 
 //casting jobLength to a number
 Number(jobLength);
 
 //ternary for job length
-jobLength > 1 ? console.log("jobLength entered correctly:" + " " + "$" + jobLength) :
+jobLength > 1 ? console.log("jobLength entered correctly:" + " " + jobLength) :
      jobLenth = prompt("Please enter again in numbers only for max :");
-
-//casting jobLength to a number
-Number(jobLength);
 
 loanLn = jobLength * .5;
 
@@ -232,13 +237,12 @@ residence = prompt("How long have you lived at your current residence in months?
 
 //residence validation
 if(residence === 0){prompt("Please in enter a number greater than Zero:")
-}else if(residence === ""){prompt("Entry left blank, please enter time at residence in months:")
-}else if(isNaN(residence)){prompt("Entery was not a number, please enter time at residence in numbers:")
-}else{alert("Time at residence:" + " " + residence + " " + "months.")}
+    }else if(residence === ""){prompt("Entry left blank, please enter time at residence in months:")
+        }else if(isNaN(residence)){prompt("Entery was not a number, please enter time at residence in numbers:")
+            }else{alert("Time at residence:" + " " + residence + " " + "months.")}
 
 //casting string to number
 Number(residence);
-
 
 //wait variable
 wait = 24 - residence;
@@ -252,6 +256,7 @@ residence >= 24 ? alert("Length of time at current residence qualifies for the l
 
 
 //TODO: credit score math w/ validation.................................................................................
+/
 //credit score determines interest rate
 
 //Getting credit score variable
@@ -279,29 +284,30 @@ console.log("creditScore number:" + " " + creditScore);
 //rate if statements
 
 //assigning creditScore variable to score
-score = creditScore;
+
 
 //rate if statements
-if(score >= 800){rate = .02
-}else if(score >= 700){rate = .04
-}else if(score >= 650){rate = .12
-}else if(score >= 550){rate = .18
-}else if(score >= 450){rate = .24
-}else if(score < 449){rate = NaN
+if(creditScore >= 800){rate = .02
+}else if(creditScore >= 700){rate = .04
+}else if(creditScore >= 650){rate = .12
+}else if(creditScore >= 550){rate = .18
+}else if(creditScore >= 450){rate = .24
+}else if(creditScore < 449){rate = NaN
 }
 
 //console log creditScore as score
-console.log("score" + " " + score);
+console.log("creditScore" + " " + creditScore);
+console.log("Rate" + " " + rate);
 
 //casting rate to a number
 Number(rate);
 
 //Credit level
-if(rate = .02){level = "Platinum"
-}else if(rate = .04){level = "Diamond"
-}else if(rate = .12){level = "Gold"
-}else if(rate = .18){level = "Silver"
-}else if(rate = .24){level = "Bronze"
+if(rate === .02){level = "Platinum"
+}else if(rate === .04){level = "Diamond"
+}else if(rate === .12){level = "Gold"
+}else if(rate === .18){level = "Silver"
+}else if(rate === .24){level = "Bronze"
 }else if(isNaN(rate)){level = "BAD"}
 
 
@@ -309,7 +315,7 @@ if(rate = .02){level = "Platinum"
 Number(creditScore);
 
 //console log credit score variable check
-console.log("creditScore number" + " " + score);
+console.log("creditScore number" + " " + creditScore);
 
 rate1 = rate;
 
@@ -338,14 +344,18 @@ if (!isNaN(rate)) {
 
 //payment calculation
 loanReal = loanMax - debt;
+
+//flat rate loan with interest applied simply
 flatRate = (rate * loanReal) + loanReal;
 
+//the flat rate loan divided by maximum loan length
 payment = flatRate / loanLn;
 
+//collateral
 collateral = checkingBal + otherBal;
 
 //loan array
-var loanArray = [payment, loanLn, rate, level];
+var loanArray = [payment, loanLn, rate, level, collateral, loanReal];
 
 
 //display user information
@@ -355,20 +365,25 @@ var loanArray = [payment, loanLn, rate, level];
     "Phone:" + " " + info[2] + "\n" +
     "Checking balance:" + " " + "$" + info[3] + "\n" +
     "Other balances total:" + " " + "$" +  info[4] + "\n" +
-    "Your" + " " + loanArray[3] + " " + "qualifies you for:" + " " + loanArray[2] +"%" + " " + "loan rate" +  "\n" +
-    "At your maximum loan term/ legnth of" + " " + loanLn + " " + "you flat loan payments would be" + " " + payment);
+    "Your" + " " + loanArray[3] + " " + "qualifies you for:" + " " + loanArray[2] +"%" + " " + "loan rate." +  "\n" +
+    "Your maximum loan amount is" + " " + "$" + loanArray[5] + " " + ", and collateral of:" + " " + "$" + loanArray[4] + "\n" +
+    "At your maximum loan term/ legnth of" + " " + loanArray[1] + " " + "months." + "\n" +
+    "your flat loan payments would be" + " " + "$" + loanArray[0]);
 
 //Displaying information via console log
 console.log("Name:" + " " + info[0] + "\n" +
-"Address:" + " " + info[1] + "\n" +
-"Phone:" + " " + info[2] + "\n" +
-"Checking balance:" + " " + "$" + info[3] + "\n" +
-"Other balances total:" + " " + "$" +  info[4] + "\n" +
-"Your" + " " + loanArray[3] + " " + "qualifies you for:" + " " + loanArray[2] +"%" + " " + "loan rate" +  "\n" +
-"At your maximum loan term/ legnth of" + " " + loanLn + " " + "you flat loan payments would be" + " " + payment);
+    "Address:" + " " + info[1] + "\n" +
+    "Phone:" + " " + info[2] + "\n" +
+    "Checking balance:" + " " + "$" + info[3] + "\n" +
+    "Other balances total:" + " " + "$" +  info[4] + "\n" +
+    "Your" + " " + loanArray[3] + " " + "qualifies you for:" + " " + loanArray[2] +"%" + " " + "loan rate." +  "\n" +
+    "Your maximum loan amount is" + " " + "$" + loanArray[5] + " " + ", and collateral of:" + " " + "$" + loanArray[4] + "\n" +
+    "At your maximum loan term/ legnth of" + " " + loanArray[1] + " " + "months." + "\n" +
+    "your flat loan payments would be" + " " + "$" + loanArray[0]);
 
 
 //values in multi-lines comment
+
 
 //compress into zip file called, Birchard_Eric_Conditionals_Assignment
 //upload to FSO
--- Birchard_Eric_Conditionals_Assignment/js/ts.js
+++ Birchard_Eric_Conditionals_Assignment/js/ts.js
@@ -1,1 +1,46 @@
 
+//credit score determines interest rate
+
+//Getting credit score variable
+creditScore = prompt("What is your current credit score?");
+
+//casting string to a number
+Number(creditScore);
+
+//Credit score validation and credit score level
+if(creditScore > 850){prompt("Please enter a number less than 851")
+}else if(creditScore < 0){prompt("Please enter a number greater than 0")
+}else if(creditScore === ""){prompt("Please enter a number between 1 and 850")
+}else if(creditScore >= 800){alert("You are at Platinum level!")
+}else if(creditScore >= 700){alert("You are at the Diamond level!")
+}else if(creditScore >= 650){alert("You are at the Gold level.")
+}else if(creditScore >= 550){alert("You are at the Silver level!")
+}else if(creditScore >= 450){alert("Bronze level.")}
+
+//casting string to a number
+Number(creditScore);
+
+//console log creditScore number
+console.log("creditScore number:" + " " + creditScore);
+
+//rate if statements
+
+//assigning creditScore variable to score
+
+
+//rate if statements
+if(creditScore >= 800){rate = .02
+}else if(creditScore >= 700){rate = .04
+}else if(creditScore >= 650){rate = .12
+}else if(creditScore >= 550){rate = .18
+}else if(creditScore >= 450){rate = .24
+}else if(creditScore < 449){rate = NaN
+}
+
+//console log creditScore as score
+console.log("creditScore" + " " + creditScore);
+console.log("Rate" + " " + rate);
+
+//casting rate to a number
+Number(rate);
+
