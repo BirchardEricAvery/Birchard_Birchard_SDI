52105077131e941dfe1d431a54c3925b13ad12a6
BirchardEricAvery <eabirchard@fullsail.edu>
2015-04-24 19:18:57 -0700

    Arguments & Parameters homeWork

--- homeWork/Functions and loops/js/loop.js
+++ homeWork/Functions and loops/js/loop.js
@@ -7,19 +7,20 @@
  Term:          I
  */
 //Functions -- normal
-//Proceedures
+//Procedures
 //Anonymous functions
 //idea  password and requirements? and user id generator number.random
 //Functions
 
 //Normal function
-
+/*
 //re-usable code or function
-var w = 5;
+//send to function arguments
+calcArea(30, 20);
 
-function calcArea(){
-    var w = 20;
-    var h = 30;
+//bins/ parameters
+
+function calcArea(w, h){
     var area = w * h;
     console.log(area);
 
@@ -28,5 +29,23 @@ function calcArea(){
 //invocation/ function call
 //using, scoped to function/ variable
 calcArea();
-console.log(w);
+console.log(area);
 //prints 5 outside as console is outside, scoped to outside w
+
+//arguments variables and parameters are where variables are stored
+    */
+
+//dog years
+
+function dogYears(age){ //parameter for storage
+    var dogYears = age * 7;
+    alert("Sparky is " + dogYears + " years old.")
+    console.log("Sparky is " + dogYears + " years old.")
+}
+//passing information/ value to first parameter
+age1 = prompt("What is Sparky's age?"); Number(age1);
+
+//argument passed in
+dogYears(age1);
+
+
