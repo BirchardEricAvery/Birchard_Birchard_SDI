df8880358d90bf30b5ef3d939a6ac0e84e70a0e2
BirchardEricAvery <eabirchard@fullsail.edu>
2015-04-29 08:32:34 -0700

    While and do While done homework done

--- homeWork/Loops/js/loops.js
+++ homeWork/Loops/js/loops.js
@@ -1,4 +1,5 @@
 /*
+
  Name:           Eric Avery Birchard
  Instructor:     Dan Williams
  Assignment:     HomeWork Loops
@@ -6,4 +7,32 @@
  Class:          SDI
  Section:        1
  Term:           I
- */
+
+
+
+TODO: While loop
+//sets up index
+var b = 10;
+
+
+//checks condition
+while(b > 0){
+    console.log(b + "Counting down:");
+
+    //increments and/ or decrements
+    b--;
+}
+
+
+*/
+
+//TODO: Do While Loop
+var c = 10;
+
+do{
+    console.log(c + " Count down the do while:");
+    c--;
+}while(c > 0);
+
+//TODO: For Loop
+
