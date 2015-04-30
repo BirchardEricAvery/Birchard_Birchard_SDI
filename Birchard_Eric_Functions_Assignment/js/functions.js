45f865287cac017f3220d073f8fb18e47c8c54f7
BirchardEricAvery <eabirchard@fullsail.edu>
2015-04-29 20:16:04 -0700

    First validation and function done

--- Birchard_Eric_Functions_Assignment/js/functions.js
+++ Birchard_Eric_Functions_Assignment/js/functions.js
@@ -11,69 +11,103 @@
 //Main code variables are not used inside of functions
 
 
-//TODO: Trailer member maker********************************************************************************************
+//TODO: Trailer member maker********************************************************************************
 
 //I for identifier, N for trailer number, E for end identifier
-//TWO*** functions
-
-//Variables
-
-
 
 
 //Begin trailer number maker prompts
 //validation loop
 //3 parameters
-var i = prompt("Enter trailer number identifier (letter/s), if none leave blank:");
+//lowercase
+var i = prompt("Enter trailer number identifier:");
+//validation
+//while loop for for blank entry for trailer number
+
+while(i === ""){
+
+    if(i === "") {
+        i = prompt("Entry was blank, please enter a trailer identifier:");
+    }else if(String(i)){
+        alert("Your trailer identifier is: " + i)
+    }
+
+
+}
+
+//making trailer identifier lower case
+i = i.toLowerCase();
 
 //Trailer number
-n = prompt("Enter trailer number:");
+var n = prompt("Enter trailer number:");
 
-//casting n to a number and validating
-Number(n);
 
 //while loop for for blank entry for trailer number
-while(isNaN(n)){
-    prompt("Entry was not a number, please enter numbers only for trailer number:")
-        }while(n === "") {prompt("Entry was left blank, " + "please enter a trailer number:")
-            }while(Number(n)){alert("Your main trailer number is: " + n)}
+
+while(n === "" || isNaN(n)){
+
+    if(n === "") {
+        n = prompt("Entry was blank, please enter numbers only for trailer number:");
+    }else if(isNaN(n)){
+        n = prompt("Please use only use numbers:")
+    }
+
+
+}
+
+//casting string n to a number
+n = parseInt(n);
 
 
 
-    //casting n to a number and validating
-            Number(n);
+var e = prompt("Enter end trailer identifier:");
+//validation
+//while loop for for blank entry for trailer end identifier
+
+while(e === ""){
+
+    if(e === "") {
+        e = prompt("Entry was blank, please enter an end trailer identifier:");
+    }else if(String(e)){
+        alert("Your trailer identifier is: " + i)
+    }
 
 
-var e = prompt("Enter end trailer identifier if any (letter/s, if none leave blank:");
+}
+
+//making trailer end identifier lower case
+e = e.toLowerCase();
 
 
 //putting function variables in to answer
-trailerNumber = trailer(i, n, e);
-var reference = 0; reference++;
+var trailerNumber = trailer(i, n, e);
 
+
+//TODO: Trailer number maker function******************************************************************************
 //making function trailer using i, n, e variables aka 3 arguments
 function trailer(i, n, e) {
     var ine;
 
-    //putting w & h into area
+    //putting i n e into trailer
     ine = i + n + e;
 
-    //function returning, or spitting out information via variable area
+    //function returning, or spitting out information via variable ine
     return ine;
 
 }
 
-
 //reporting answer via console log
-alert("Your trailer is: " + trailerNumber + "\n" +
- " and reference number is: " + reference);
+alert("Your trailer is: " + trailerNumber);
+
+//console logging trailer number created
 console.log("Trailer number created" + " " + trailerNumber);
-console.log("Reference number: " + reference);
+
 
 
 /*
-//TODO: Trailer location identifier*************************************************************************************
-//prompt for location
+//TODO: Trailer location function**********************************************************************************
+//prompt for location maker
+ //lowercase
 var i2 = prompt("Enter location number identifier (letter/s), if none leave blank:");
 
 //Trailer number
@@ -84,15 +118,15 @@ Number(n2);
 
 if(n2 === "") {
     prompt("Entry was left blank, " +
-        "please enter a trailer number:")
+        "please enter a location number:")
 }else if(isNaN(n2)) {
-    n = prompt("Please enter numbers only for trailer number:")
-}else{alert("Trailer number: " + n)}
+    n = prompt("Please enter numbers only for location number:")
+}else{alert("Location number: " + n)}
 
 //while loop for for blank entry for trailer number
-while(n === ""){
+while(n2 === ""){
     prompt("Entry was left blank, " +
-        "please enter a trailer number:")
+        "please enter a location number:")
 }
 
 //while loop for trailer number if not a number
@@ -107,48 +141,42 @@ Number(n2);
 var e2 = prompt("Enter end trailer identifier if any (letter/s, if none leave blank:");
 
 
-//putting function variables in to answer
-trailerNumber = trailer(i2, n2, e2);
-var reference = 0; reference++;
+//putting function variables in to reference2
+locationNumber = trailer(i2, n2, e2, reference);
+var reference2 = 0; reference2++;
 
 //making function trailer using i, n, e variables aka 3 arguments
-function trailer(i2, n2, e2) {
+function trailer(i2, n2, e2, reference) {
     var ine2;
 
     //putting w & h into area
-    ine = i + n + e;
+    ine = i2 + n2 + e2 + reference;
 
     //function returning, or spitting out information via variable area
-    return ine;
+    return ine2;
 
 }
 
-
-
-
-//TWO*** functions
-//Validate
-
-//TODO: Trailer location description added to trailer
-//Validate
-
-
-
-//TODO: Chose R for recall, M for modify
-//Validate
-
-//TODO: list all trailers and references
-
-//TODO: List all locations and references
-
-
-//Required
+*/
 
 
 
 
 
-//Anonymous function
+
+//anonymous function has to come after function
+//combine trailer number and location
+
+/*
+var addNums = function(num1, num2){
+    var total = Number(num1)+Number(num2);
+    return total;
+};
+//call
+var totalReturned = addNums(5, 6);
+console.log(totalReturned + " returned");
+*/
+//array display trailer, location, combined
 
 //write end test values
 //5 commits, verify in get hub, send link to instructor
